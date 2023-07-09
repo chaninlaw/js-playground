9513fdbbcdb124b9acfd97bd41fa4b1f6d710d4a
Chanin Law <chaninlawlert@gmail.com>
2023-07-09 20:42:23 +0700

    feat: implement esbuild tranform api(transpile)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import * as esbuild from "esbuild-wasm";
+import { Service } from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
 
-type Props = {};
+const App: React.FC = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<Service>();
 
-const App: React.FC<Props> = (props) => {
-  return <div>App</div>;
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService().catch((err) => console.error(err));
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
+  };
+
+  return (
+    <>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        cols={30}
+        rows={10}
+      />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </>
+  );
 };
 
 export default App;
