0b846edff48f774efa678cc85521a4c29d0830e6
Chanin Law <chaninlawlert@gmail.com>
2023-07-10 23:39:04 +0700

    feat: solve relative path on require statement

--- src/plugins/unpgk-path-plugin.ts
+++ src/plugins/unpgk-path-plugin.ts
@@ -1,39 +1,64 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
-    // good for dubugging with many plugin on esbuild
+    // name key is good for dubugging with many plugin on esbuild
     name: "unpkg-path-plugin",
 
     // bundling process attaching onResolve and onLoad event
     setup(build: esbuild.PluginBuild) {
       build.onResolve(
         { filter: /.*/ }, // filter to check type of file would like to executed
-        (args: esbuild.OnResolveArgs): esbuild.OnResolveResult => {
+        async (
+          args: esbuild.OnResolveArgs
+        ): Promise<esbuild.OnResolveResult> => {
           console.log("onResolve", args);
-          return { path: args.path, namespace: "a" }; // namespace similar filter that specific name of file where you wanna execute
+
+          if (args.path === "index.js") {
+            return { path: args.path, namespace: "a" }; // namespace similar filter that specific name of file where you wanna execute
+          }
+
+          if (args.path.includes("./") || args.path.includes("../")) {
+            return {
+              namespace: "a",
+              path: new URL(args.path, args.importer + "/").href,
+            };
+          }
+
+          return {
+            namespace: "a",
+            path: `https://unpkg.com/${args.path}`,
+          };
+          // else if (args.path === "tiny-test-pkg") {
+          //   return {
+          //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+          //     namespace: "a",
+          //   };
+          // }
         }
       );
 
       build.onLoad(
         { filter: /.*/ },
-        (args: esbuild.OnLoadArgs): esbuild.OnLoadResult => {
+        async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
           console.log("onLoad", args);
 
           if (args.path === "index.js") {
             return {
               loader: "jsx",
               contents: `
-              import message from './message';
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
             };
-          } else {
-            return {
-              loader: "jsx",
-              contents: 'export default "hi there!!!"',
-            };
           }
+
+          const { data } = await axios.get(args.path);
+          return {
+            loader: "jsx",
+            contents: data,
+          };
         }
       );
     },
