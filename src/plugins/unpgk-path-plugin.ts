54b88f7aeef9e180b87d5276764b12a6c7992d7f
Chanin Law <chaninlawlert@gmail.com>
2023-07-23 22:24:06 +0700

    refactor: onResolve filter using RE

--- src/plugins/unpgk-path-plugin.ts
+++ src/plugins/unpgk-path-plugin.ts
@@ -8,36 +8,28 @@ const fileCache = localforage.createInstance({
 
 export const unpkgPathPlugin = (inputCode: string) => {
   return {
-    // name key is good for dubugging with many plugin on esbuild
     name: "unpkg-path-plugin",
-
-    // bundling process attaching onResolve and onLoad event
     setup(build: esbuild.PluginBuild) {
-      build.onResolve(
-        // filter to check type of file would like to executed
-        { filter: /.*/ },
-        async (args) => {
-          console.log("onResolve", args);
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
 
-          if (args.path === "index.js") {
-            return { path: args.path, namespace: "a" };
-          }
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+        };
+      });
 
-          if (args.path.includes("./") || args.path.includes("../")) {
-            return {
-              // namespace similar filter that specific name of file where you wanna execute
-              namespace: "a",
-              path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-                .href,
-            };
-          }
-
-          return {
-            namespace: "a",
-            path: `https://unpkg.com/${args.path}`,
-          };
-        }
-      );
+      // Handle main file of module
+      build.onResolve({ filter: /.*/ }, async (args) => {
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
 
       build.onLoad({ filter: /.*/ }, async (args) => {
         console.log("onLoad", args);
@@ -48,6 +40,7 @@ export const unpkgPathPlugin = (inputCode: string) => {
             contents: inputCode,
           };
         }
+
         // Cache Layer
         // Check to see if we have already fetched this file
         // and if it's in ther cache
