51ed833a3c72cdaa800b9e760ff08436a895a04a
Chanin Law <chaninlawlert@gmail.com>
2023-08-29 23:19:13 +0700

    add base route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './state'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 const el = document.getElementById('root')
 
 createRoot(el!).render(
   <Provider store={store}>
+    <BrowserRouter basename={import.meta.env.DEV !== 'production' ? '/' : '/js-playground/'}>
     <App />
+    </BrowserRouter>
   </Provider>
 )
