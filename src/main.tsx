485fef7b0164745292eebdc5583be865aa52851e
Chanin Law <chaninlawlert@gmail.com>
2023-08-30 19:00:04 +0700

    fix: deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './state'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
 
 const el = document.getElementById('root')
 
 createRoot(el!).render(
   <Provider store={store}>
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/js-playground/'}>
     <App />
-    </BrowserRouter>
   </Provider>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    server: {
-      port: 3000,
-    },
-    preview: {
-      port: 8080,
-    },
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/js-playground/";
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  base: '/js-playground/'
 });
