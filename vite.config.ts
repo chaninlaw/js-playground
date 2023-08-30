1ab4b03c1c6aff9ed35a25608ad7fc628a5b4653
Chanin Law <chaninlawlert@gmail.com>
2023-08-30 19:21:15 +0700

    fix: base route

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/js-playground/'
+export default defineConfig(({ command }) => {
+  const config = {
+    server: {
+      port: 3000,
+    },
+    preview: {
+      port: 8080,
+    },
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/js-playground/";
+  }
+
+  return config;
 });
