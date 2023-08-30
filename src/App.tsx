2f248b951a0b356244cfc7bbeaa5d5c0ce5cfd09
Chanin Law <chaninlawlert@gmail.com>
2023-08-30 19:25:27 +0700

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import CellList from './components/CellList'
 
 const App: React.FC = () => {
-  return (
-    <>
-      <CellList />
-    </>
-  )
+  return <CellList />
 }
 
 export default App
